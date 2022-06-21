6fbd6aa5d396ab641d94473a38623e231000aca0
Andre Gomez <andregc1983@gmail.com>
2022-06-20 21:51:10 -0300

    Implementação dos Endpoints de Forma de Pagamentos

---
+++ src/main/java/com/algaworks/algafood/api/model/FormaPagamentoModel.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class FormaPagamentoModel { //DTO
+
+    private Long id;
+    private String descricao;
+    
+}  
